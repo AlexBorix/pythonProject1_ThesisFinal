10281184b1f8c392fbf32cbdb86bc2497928d901
BigMindset2023 <Africa.Innov2023@gmail.com>
2024-07-08 15:25:17 +0000

    third commit

---
+++ AddDataToDatabase.py
@@ -0,0 +1,60 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred,{
+    'databaseURL':"https://faceattendancethesisproject-default-rtdb.firebaseio.com/"
+})
+
+ref = db.reference('Students')
+
+data = {
+    "00001" :
+        {
+            "name": "Mme SOUDRE",
+            "major": "IT",
+            "starting_year": 2015,
+            "total_attendance": 3,
+            "standing": "B",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34",
+
+        },
+"321654" :
+        {
+            "name": "Hamado",
+            "major": "Dev",
+            "starting_year": 2017,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2022-12-11 00:54:34",
+
+        },
+"852741" :
+        {
+            "name": "Naomi",
+            "major": "Mgm",
+            "starting_year": 2021,
+            "total_attendance": 12,
+            "standing": "B",
+            "year": 1,
+            "last_attendance_time": "2022-12-11 00:54:34",
+
+        },
+"963852" :
+        {
+            "name": "Boris",
+            "major": "Thesis",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34",
+
+        },
+}
+
+for key,value in data.items():
+    ref.child(key).set(value) #we use child because it is a specific directory
--- main.py
+++ main.py
@@ -4,6 +4,20 @@ import numpy as np
 import cv2
 import face_recognition
 import cvzone
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+import  numpy as np
+from datetime import datetime
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred,{
+    'databaseURL': "https://faceattendancethesisproject-default-rtdb.firebaseio.com/",
+    'storageBucket': "faceattendancethesisproject.appspot.com"
+})
+
+bucket = storage.bucket()
 
 cap = cv2.VideoCapture(0)
 cap.set(3,640)
@@ -28,6 +42,11 @@ encodeListKnown, studentIds = encodeListKnownWithIds
 #print(studentIds)
 print('Encode File Loaded.......')
 
+modeType = 0
+counter = 0
+id = -1
+imgStudent = []
+
 while True:
     success, img = cap.read()
 
@@ -38,26 +57,104 @@ while True:
     encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
 
     imgBackground[162:162+480, 55:55+640] = img
-    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[3]
+    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType] #modeType to display the different mode when the face is detected
 
-    for encodeFace, faceLoc in zip(encodeCurFrame,faceCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        #print("Matches", matches)
-        #print("faceDIs", faceDis)
+    if faceCurFrame:
+        for encodeFace, faceLoc in zip(encodeCurFrame,faceCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+            #print("Matches", matches)
+            #print("faceDIs", faceDis)
 
-        matchIndex = np.argmin(faceDis)
-        #print("matchIndex", matchIndex)
+            matchIndex = np.argmin(faceDis)
+            #print("matchIndex", matchIndex)
 
-        if matches[matchIndex]:
-            #print("Know Face Detected")
-            #print(studentIds[matchIndex]) # to see the index of the face detected
-            y1, x2, y2, x1 = faceLoc
-            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-            bbox = 55 + x1, 162 + y1, x2 - x1, y2 - y1
-            imgBackground = cvzone.cornerRect(imgBackground,bbox,rt=0)
+            if matches[matchIndex]:
+                #print("Know Face Detected")
+                #print(studentIds[matchIndex]) # to see the index of the face detected
+                y1, x2, y2, x1 = faceLoc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                bbox = 55 + x1, 162 + y1, x2 - x1, y2 - y1
+                imgBackground = cvzone.cornerRect(imgBackground,bbox,rt=0) #but a rectangle around the face detected
+                id = studentIds[matchIndex]
+                if counter == 0:
+                    cvzone.putTextRect(imgBackground,"Loading", (275, 400))
+                    cv2.imshow("Face Attendance", imgBackground)
+                    counter = 1 #after detected the face counter become 1
+                    modeType = 1
+        if counter != 0:
+
+            if counter == 1:
+                #Get the data
+                studentInfo = db.reference(f'Students/{id}').get() #we want to get the info of the student face detected
+                print(studentInfo)
+                #Get the image from the storage
+                blob = bucket.get_blob(f'Images/{id}.png')
+                array = np.frombuffer(blob.download_as_string(), np.uint8)
+                imgStudent = cv2.imdecode(array, cv2.COLOR_BGRA2BGR)
+                #Update data of attendance
+                datetimeObject = datetime.strptime(studentInfo['last_attendance_time'],
+                                                 "%Y-%m-%d %H:%M:%S")
+                secondsElapsed = (datetime.now()-datetimeObject).total_seconds()
+                print(secondsElapsed)
+                if secondsElapsed > 30:
+                    ref = db.reference(f'Students/{id}')
+                    studentInfo['total_attendance'] += 1
+                    ref.child('total_attendance').set(studentInfo['total_attendance']) #upadate in the database the total attendance
+                    ref.child('last_attendance_time').set(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                else:
+                    modeType = 3
+                    counter = 0
+                    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+
+            if modeType != 3:
+
+
+                if 10 <counter<= 20:
+                    modeType = 2
+                imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+                if counter <=10:
+                    #dissplay the different information of the student
+                    cv2.putText(imgBackground,str(studentInfo['total_attendance']),(861,125),
+                                cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1) #to display the number of attendance on the background
+                    cv2.putText(imgBackground, str(studentInfo['major']), (1006, 550),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+                    cv2.putText(imgBackground, str(id), (1006, 493),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+                    cv2.putText(imgBackground, str(studentInfo['standing']), (910, 625),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+                    cv2.putText(imgBackground, str(studentInfo['year']), (1025, 625),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+                    cv2.putText(imgBackground, str(studentInfo['starting_year']), (1125, 625),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+
+                    #we wanna center the name
+                    (w, h), _ = cv2.getTextSize(studentInfo['name'],cv2.FONT_HERSHEY_COMPLEX,1,1) #get the weight of the name text
+                    offset = (414-w)//2
+                    cv2.putText(imgBackground, str(studentInfo['name']), (808+offset, 445),
+                                cv2.FONT_HERSHEY_COMPLEX, 1, (50, 50, 50), 1)
+
+                    #the size of my image in the data is more big than the place reserved in the background so i resize the img before displaying it
+                    # Exemple de redimensionnement de imgStudent à la taille attendue
+                    imgStudent = cv2.resize(imgStudent, (216, 216))
+                    imgBackground[175:175 + 216, 909:909 + 216] = imgStudent
+
+                counter += 1
+
+                if counter >= 20 :
+                    counter = 0
+                    modeType = 0
+                    studentInfo = []
+                    imgStudent = []
+                    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+    else:
+        modeType = 0
+        counter = 0
+
+
 
     #cv2.imshow("Computer Camera",img)
     cv2.imshow("Face Attendance", imgBackground)
     cv2.waitKey(1)
     ###real
+
