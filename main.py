ad98e5679654c83344d6743685c6573891196a61
BigMindset2023 <Africa.Innov2023@gmail.com>
2024-07-08 09:57:29 +0000

    second commit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 import pickle
-
+import numpy as np
 import cv2
 import face_recognition
+import cvzone
 
 cap = cv2.VideoCapture(0)
 cap.set(3,640)
@@ -31,7 +32,7 @@ while True:
     success, img = cap.read()
 
     imgS = cv2.resize(img,(0, 0),None, 0.25, 0.25)
-    imgS = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
     faceCurFrame = face_recognition.face_locations(imgS)
     encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
@@ -42,10 +43,21 @@ while True:
     for encodeFace, faceLoc in zip(encodeCurFrame,faceCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        print("Matches", matches)
-        print("Face distance", faceDis)
+        #print("Matches", matches)
+        #print("faceDIs", faceDis)
 
+        matchIndex = np.argmin(faceDis)
+        #print("matchIndex", matchIndex)
+
+        if matches[matchIndex]:
+            #print("Know Face Detected")
+            #print(studentIds[matchIndex]) # to see the index of the face detected
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            bbox = 55 + x1, 162 + y1, x2 - x1, y2 - y1
+            imgBackground = cvzone.cornerRect(imgBackground,bbox,rt=0)
 
     #cv2.imshow("Computer Camera",img)
     cv2.imshow("Face Attendance", imgBackground)
     cv2.waitKey(1)
+    ###real
